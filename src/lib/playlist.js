59d306399ded6b2fdd1fcf6af01c3c0eb4579ed3
Diyibo <chessusuario1230@gmail.com>
2025-02-26 18:06:48 -0500

    Fix: Improve how to get the data from playlists

--- src/lib/playlist.js
+++ src/lib/playlist.js
@@ -1,3 +1,5 @@
+import { Playlist } from "../playlists/models/playlist.model"
+
 export const fetchPlaylists = async() => {
   
   try {
@@ -13,4 +15,26 @@ export const fetchPlaylists = async() => {
     throw error
   }
   
+}
+
+/**
+ * 
+ * @param {string} playlistId 
+ * @returns {Promise<Playlist>|Error}
+ */
+export const fetchPlaylistById = async( playlistId = '2' ) => {
+
+  try {
+    const response = await fetch(`http://localhost:3020/playlists/${playlistId}`)
+
+    if(!response.ok) {
+      throw new Error(`Error: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
+  } catch(error) {
+    throw error
+  }
+
 }
