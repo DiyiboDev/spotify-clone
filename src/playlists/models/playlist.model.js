f072255b2b1dedb4acacd958c2ac71826b16c9fe
Diyibo <chessusuario1230@gmail.com>
2025-03-01 20:51:37 -0500

    Refactor: Update the keys of the Playlist

--- src/playlists/models/playlist.model.js
+++ src/playlists/models/playlist.model.js
@@ -6,14 +6,18 @@ export class Playlist {
    * @param {string} title 
    * @param {string} color 
    * @param {string} cover 
+   * @param {string} coverArtist
    * @param {Array<string>} artists 
+   * @param {Array<string>} songs
    */
-  constructor( id, albumId, title, color, cover, artists = [] ) {
+  constructor( id, albumId, title, color, cover, coverArtist, artists = [], songs = [] ) {
     this.id = id
     this.albumId = albumId
     this.title = title
     this.color = color
     this.cover = cover
+    this.coverArtist = coverArtist
     this.artists = artists
+    this.songs = songs
   }
 }
