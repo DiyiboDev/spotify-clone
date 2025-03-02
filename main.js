6772373da4a26ccbcd4af854c8e865c5669ad49b
Diyibo <chessusuario1230@gmail.com>
2025-03-01 20:42:17 -0500

    Chore: Message for the developer

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ import { App } from './src/playlists/main'
 import './src/style.css'
 
 App()
+
+// TODO: FIX WHEN THE URL CHANGE ALSO RENDER THE PLAYLIST AND THE PLAYLISTS (CHANGE THE NAMES FOR MORE CLARITY)
